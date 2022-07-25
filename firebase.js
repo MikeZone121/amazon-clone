ad8e96a8b78e0bd203c6fb495495315e562e97e8
Mike Livis <mikezone1805@gmail.com>
2022-07-25 16:05:26 +0200

    feat: added checkoutpage

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAi6iTiHKxO6g2IDNo1vek5jw7TlDU5SHQ",
+    authDomain: "fir-a8532.firebaseapp.com",
+    projectId: "fir-a8532",
+    storageBucket: "fir-a8532.appspot.com",
+    messagingSenderId: "617008360344",
+    appId: "1:617008360344:web:7305ff27e6e8227b2ed9ed"
+  };
--- src/slices/basketSlice.js
+++ src/slices/basketSlice.js
@@ -8,8 +8,27 @@ export const basketSlice = createSlice({
   name: "basket",
   initialState,
   reducers: {
-    addToBasket: (state, action) => {},
-    removeFromBasket: (state, action) => {},
+
+    //Actions
+    addToBasket: (state, action) => {
+      state.items = [...state.items, action.payload]
+    },
+    removeFromBasket: (state, action) => {
+      const index = state.items.findIndex((basketItem) => basketItem.id === action.payload.id);
+
+      // Kopie van basket
+      let newBasket = [...state.items];
+
+      if (index >= 0){
+        //Item exist in basket... Remove it...
+        newBasket.splice(index, 1)
+      } else {
+        console.warn(`Cant remove product (id: ${action.payload.id}) as its not resist`)
+      }
+
+      state.items = newBasket;
+
+    },
   },
 });
 
@@ -17,5 +36,6 @@ export const { addToBasket, removeFromBasket } = basketSlice.actions;
 
 // Selectors - This is how we pull information from the Global store slice
 export const selectItems = (state) => state.basket.items;
+export const selectTotal = (state) => state.basket.items.reduce((total, item) => total + item.price ,0);
 
 export default basketSlice.reducer;
